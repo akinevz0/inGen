af9c1292d1c58ca6b5b9eaf8e4e1147044ed02b6
Kirill kine Nevzorov <akinevz@outlook.com>
2024-07-30 16:52:18 +0000

    add logger to ManagedInstall

--- src/main/java/com/akinevz/install/platform/ManagedInstall.java
+++ src/main/java/com/akinevz/install/platform/ManagedInstall.java
@@ -1,11 +1,15 @@
 package com.akinevz.install.platform;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.akinevz.install.package_manager.IPackageManager;
 import com.akinevz.install.package_manager.PackageInstallException;
 
 public class ManagedInstall implements IPackageManager {
+
+    private final static Logger logger = Logger.getLogger(ManagedInstall.class.getName());
 
     private final String installName;
     private final ManagedInstallAdapter packages;
@@ -27,6 +31,7 @@ public class ManagedInstall implements IPackageManager {
 
     @Override
     public void install(final String packageName) throws PackageInstallException {
+        logger.log(Level.WARNING, "Registering locally installed package {0}", packageName);
         throw new UnsupportedOperationException("Unsupported method 'install' for a managed install");
     }
 
